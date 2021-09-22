bd3e33267d707e9132bedadd21ce7f1cbd6e417d
ACristoff <acristoffanini@gmail.com>
2021-09-22 23:38:55 +0000

    added some edge cases, almost finished

--- fetcher.js
+++ fetcher.js
@@ -2,25 +2,41 @@ const request = require('request');
 const fs = require('fs');
 let content = "";
 
-request('http://www.example.edu/4ghgjfdh', (error, response, body) => {
+request('http://www.example.edu', (error, response, body) => {
   // console.log('error:', error); // Print the error if one occurred
   // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   // console.log('body:', body); // Print the HTML for the Google homepage.
   // console.log(body.length)
   content = body;
+  let path = './test.txt';
 
   if (response.statusCode !== 200) {
     console.log(`Invalid HTTP address, please correct the URL. Error code: ${response.statusCode}`)
     return
   }
 
-  fs.writeFile('./test.txt', content, err => {
+  //asynchronous, works but need to implement the rewrite
+  // fs.access(path, fs.F_OK, (err) => {
+  //   if (!err) {
+  //     console.log('This file already exists! Rename file?')
+  //     return
+  //   }
+  // });
+  
+  //finish this later
+  try {
+    if (fs.existsSync(path)) {
+      //file exists
+      console.log('file exists')
+    }
+  } catch(err) {
+    console.error(err)
+  }
+
+  fs.writeFile(path, content, err => {
     if (err) {
       console.error(err)
       return
-    }
-    if ('/test.txt') {
-      console.log('test.txt already exists!')
     }
 
     console.log(`Downloaded and saved ${body.length} bytes to ./index.html`)
@@ -28,11 +44,3 @@ request('http://www.example.edu/4ghgjfdh', (error, response, body) => {
 });
 
 //Downloaded and saved 3261 bytes to ./index.html
-
-// fs.writeFile('/Users/joe/test.txt', content, err => {
-//   if (err) {
-//     console.error(err)
-//     return
-//   }
-//   //file written successfully
-// })
